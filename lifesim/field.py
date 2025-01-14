2e39d623f4c14c25f4feb717e21ea4e9798c7b84
seiko.iwasawa <seiko.iwasawa@gmail.com>
2025-01-14 13:05:33 +0300

    improve field rules

--- lifesim/field.py
+++ lifesim/field.py
@@ -16,26 +16,29 @@ class Field:
             color=config.BACKGROUND_COLOR,
             batch=batch,
         )
-        self.food: list[Food] = []
+        self.food: list[Food] = [
+            util.gen_food(self.batch) for n in range(config.FOOD_N)
+        ]
         self.fishes = [util.gen_fish(self.batch) for n in range(config.FISH_N)]
-
-    def gen_food(self):
-        self.food.append(util.gen_food(self.batch))
 
     def update(self, dt: float):
         for fish in self.fishes:
             fish.update(self.food, self.fishes, dt)
-        if len(self.food) < config.FOOD_N:
-            self.gen_food()
-        self.intersect()
+        while self.intersect():
+            ...
 
     def intersect(self):
-        new_food: list[Food] = []
-        for food in self.food:
-            flag = True
-            for fish in self.fishes:
+        for fish in self.fishes:
+            for other in self.fishes:
+                if (
+                    fish != other
+                    and util.dist(fish, other) <= fish.radius + other.radius
+                ):
+                    self.fishes.pop(self.fishes.index(other))
+                    return True
+        for fish in self.fishes:
+            for food in self.food:
                 if util.dist(fish, food) <= fish.radius + food.radius:
-                    flag = False
-            if flag:
-                new_food.append(food)
-        self.food = new_food
+                    self.food.pop(self.food.index(food))
+                    return True
+        return False
